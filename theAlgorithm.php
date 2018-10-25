6371183cd36fb2494698ffce03d26b41d6315af4
Andrew Meyer <42983535+noCanDrew@users.noreply.github.com>
2018-10-25 13:54:52 -0700

    Add files via upload

--- theAlgorithm.php
+++ theAlgorithm.php
@@ -4,106 +4,106 @@
 		Returns the graph complement of a simple graph input
 	*/
 	function invertGraph($g){
-    	for($a = 0; $a < count($g); $a+=1){
-    		for($b = 0; $b < count($g); $b+=1){
-    			if($g[$a][$b] == 0) $g[$a][$b] = 1;
-                else $g[$a][$b] = 0;
-    		}
-    	}
-    	return $g;
-    }
+		for($a = 0; $a < count($g); $a+=1){
+			for($b = 0; $b < count($g); $b+=1){
+				if($g[$a][$b] == 0) $g[$a][$b] = 1;
+				else $g[$a][$b] = 0;
+			}
+		}
+		return $g;
+	}
 
-    /*
+	/*
 		Returns number of edges in graph
-    */
+	*/
 	function edgeCount($g){
-    	$count = 0;
-    	for($a = 0; $a < count($g); $a+=1){
-    		for($b = 0; $b < count($g); $b+=1){
-    			if($g[$a][$b] != 0) $count += 1;
-    		}
-    	}
-    	return $count;  
-    }
+		$count = 0;
+		for($a = 0; $a < count($g); $a+=1){
+			for($b = 0; $b < count($g); $b+=1){
+				if($g[$a][$b] != 0) $count += 1;
+			}
+		}
+		return $count;  
+	}
 
-    /*
+	/*
 		- Returns true if graph is bipartite.
 		- Returns false if graph is not bipartite.
 
 		- NOTE: Used BFS approach described at:
 		geeksforgeeks.org/check-graphs-cycle-odd-length/
 	*/
-    function isBipartite($g){
-    	for($a = 0; $a < count($g); $a+=1){
-    		if($g[$a][$a] == 1) return false;
-    	}
+	function isBipartite($g){
+		for($a = 0; $a < count($g); $a+=1){
+			if($g[$a][$a] == 1) return false;
+		}
 
-    	$colorArr = array();
-    	for($a = 0; $a < count($g); $a+=1) $colorArr[$a] = -1;
+		$colorArr = array();
+		for($a = 0; $a < count($g); $a+=1) $colorArr[$a] = -1;
 
-    	$colorArr[0] = 1;
-    	$q = array();
-        array_push($q, 0);
+		$colorArr[0] = 1;
+		$q = array();
+		array_push($q, 0);
 
-        while(!empty($q)){
-        	$u = array_shift($q);
-        	for($a = 0; $a < count($g); $a+=1){
-        		if($g[$u][$a] != 0 && $colorArr[$a] == -1){
-        			$colorArr[$a] = 1 - $colorArr[$u];
-        			array_push($q, $a);
-        		} elseif($g[$u][$a] != 0 && $colorArr[$a] == $colorArr[$u]){ 
-        			return false;
-        		}
-        	}
-        }
-    	return true;
-    }
+		while(!empty($q)){
+			$u = array_shift($q);
+			for($a = 0; $a < count($g); $a+=1){
+				if($g[$u][$a] != 0 && $colorArr[$a] == -1){
+					$colorArr[$a] = 1 - $colorArr[$u];
+					array_push($q, $a);
+				} elseif($g[$u][$a] != 0 && $colorArr[$a] == $colorArr[$u]){ 
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
-    /*
+	/*
 		- Complete bipartite graphs have e = |v1| * |v2| edges where v1 and v2 
 		are the two bipartitions of vertices in G.
 		- This function returns an array with three values. 
 			- The first element is true if the input graph is complete bipartite.
 			- The second is the size of the first bipartition of the input graph.
 			- The third is the size of the second bipartition of the input graph. 
-    */
+	*/
 	function completeBipartite($g){
 		$colorArr = array();
-    	for($a = 0; $a < count($g); $a+=1) $colorArr[$a] = -1;
+		for($a = 0; $a < count($g); $a+=1) $colorArr[$a] = -1;
 
-    	$colorArr[0] = 1;
-    	$q = array();
-        array_push($q, 0);
+		$colorArr[0] = 1;
+		$q = array();
+		array_push($q, 0);
 
-        while(!empty($q)){
-        	$u = array_shift($q);
-        	for($a = 0; $a < count($g); $a+=1){
-        		if($g[$u][$a] != 0 && $colorArr[$a] == -1){
-        			$colorArr[$a] = 1 - $colorArr[$u];
-        			array_push($q, $a);
-        		} elseif($g[$u][$a] != 0 && $colorArr[$a] == $colorArr[$u]){
-        			return false;
-        		}
-        	}
-        }
-        $reds = 0;
-        for($a = 0; $a < count($colorArr); $a+=1) if($colorArr[$a] == 1) $reds+=1;
-        if(edgeCount($g)/2 == $reds*(count($colorArr) - $reds)) return true;
+		while(!empty($q)){
+			$u = array_shift($q);
+			for($a = 0; $a < count($g); $a+=1){
+				if($g[$u][$a] != 0 && $colorArr[$a] == -1){
+					$colorArr[$a] = 1 - $colorArr[$u];
+					array_push($q, $a);
+				} elseif($g[$u][$a] != 0 && $colorArr[$a] == $colorArr[$u]){
+					return false;
+				}
+			}
+		}
+		$reds = 0;
+		for($a = 0; $a < count($colorArr); $a+=1) if($colorArr[$a] == 1) $reds+=1;
+		if(edgeCount($g)/2 == $reds*(count($colorArr) - $reds)) return true;
 		else return false;
 	}
 
-    /*
+	/*
 		- A bad joke
 		- See Lemma 4.3's note for justification
-    */
-    function completeTheGraph($g){
-    	for($a = 0; $a < count($g); $a+=1){
-    		for($b = 0; $b < count($g); $b+=1){
-    			if($g[$a][$b] == 0) $g[$a][$b] = 8;
-    		}
-    	}
-    	return $g;
-    }
+	*/
+	function completeTheGraph($g){
+		for($a = 0; $a < count($g); $a+=1){
+			for($b = 0; $b < count($g); $b+=1){
+				if($g[$a][$b] == 0) $g[$a][$b] = 8;
+			}
+		}
+		return $g;
+	}
 
 	///////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////
@@ -111,7 +111,7 @@
 	///////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////
 
-    /*
+	/*
 		- Input graphs are assumed to be connected graphs
 		- Returned array includes:
 			- 1 if isomorphic, 0 if not
@@ -124,7 +124,7 @@
 		- Calls canonicalNameCompare() on inversions of a,b if a,b are both bipartite, 
 		  not complete bipartite, and are invarient for edges, vertices
 		- Returns false if one of the input graphs is bipartite but the other is not
-    */
+	*/
 	function GI($a, $b){
 		$a = mux($a); // from graphBuilder.php
 		$b = mux($b);		
