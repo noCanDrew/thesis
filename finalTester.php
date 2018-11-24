bdb053ab4c8d2255b5b5c697df07e521d7d290ac
Andrew Meyer <42983535+noCanDrew@users.noreply.github.com>
2018-11-23 16:22:45 -0800

    Add files via upload

--- finalTester.php
+++ finalTester.php
@@ -9,11 +9,11 @@
 	function getMatrixString($m){
 		$str = "";
 		for($a = 0; $a < count($m); $a+=1){ 
-	    	for($b = 0; $b < count($m); $b+=1){
-	      		$str .= ',' . $m[$a][$b];
-	      	}
-	  	}
-	  	return $str;
+			for($b = 0; $b < count($m); $b+=1){
+				$str .= ',' . $m[$a][$b];
+			}
+		}
+		return $str;
 	}
 
 	if(isset($_GET['hash']) && $_GET['hash'] =='someRandomString'){
